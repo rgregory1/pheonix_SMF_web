56630cf7189165182409664b987042254ac1ef87
rusti <mrgregory1@gmail.com>
2018-10-12 14:30:37 -0400

    start of more methods

--- smf_web.py
+++ smf_web.py
@@ -1,15 +1,23 @@
 import json
-# import jsonpickle
+import pprint
 
 class Dog:
 
     def __init__(self, name):
         self.name = name
         self.tricks = []    # creates a new empty list for each dog
+        self.age = 4
+        self.training = 0
+        self.appetite = 4
+        self.speed = 6
+
 
     def add_trick(self, trick):
         self.tricks.append(trick)
 
+    def change_name(self, name):
+        self.name = name
+            
     @classmethod
     def init_from_json_str(cls, json_str):
         m = cls(None)
@@ -17,36 +25,32 @@ class Dog:
         return m
 
     def print_dog(self):
+        print('------------ ', self.name, ' --------------')
         for attr, value in self.__dict__.items():
             print(attr, '=' ,value)
+        print('\n\n')
 
 Fido = Dog('Fido')
 print('\n\n')
 print(Fido.name)
 
-Fido.add_trick('roll over')
-print(Fido.tricks)
+Fido.print_dog()
 
-s = json.dumps(Fido.__dict__)
-print('\n\n')
-print(s)
-print('\n\n')
 
-Spot = Dog.init_from_json_str(s)
-# Spot = Dog()
-# Spot.init_from_json_str(s)
 
-print(Spot.name)
-
-print(Spot.print_dog())
-
-print(Spot)
-# s = jsonpickle.encode(Fido)
+# # Json dump testing
+# Fido.add_trick('roll over')
+#
+# print(Fido.tricks)
+#
+# s = json.dumps(Fido.__dict__)
 #
 # print(s)
 #
-# Spot = jsonpickle.decode(s)
+# Spot = Dog.init_from_json_str(s)
 #
-# Spot.add_trick('shake hands')
+# Spot.change_name('Spot')
 #
-# print(Spot.tricks)
+# print(Spot.name)
+#
+# Spot.print_dog()
